0a0fc1d1f532c9ce51930cce314865ef74faa13f
Juergen Stuber <juergen@jstuber.net>
2018-12-09 12:24:55 +0100

    Add day 9.

---
+++ src/bin/a201809a.rs
@@ -0,0 +1,66 @@
+use std::io;
+use std::iter::repeat;
+use std::str::FromStr;
+
+use nom::call;
+use nom::digit;
+use nom::do_parse;
+use nom::error_position;
+use nom::map_res;
+use nom::named;
+use nom::tag;
+use nom::tag_s;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Config {
+    n_players: i32,
+    last_marble: i32,
+}
+
+// 459 players; last marble is worth 71790 points
+
+named!(int32<&str, i32>,
+    map_res!(digit, FromStr::from_str)
+);
+
+named!(config<&str, Config>,
+       do_parse!(
+           n_players: int32
+               >> tag_s!(" players; last marble is worth ")
+               >> last_marble: int32
+               >> tag_s!(" points")
+               >> (Config{ n_players, last_marble })
+       )
+);
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("I/O error");
+
+    // parse line
+    let result = config(&line.trim_end());
+    //println!("{:?}", result);
+
+    let config = result.unwrap().1;
+
+    let n_players = config.n_players as usize;
+    let last_marble = config.last_marble;
+
+    let mut players = repeat(0).take(n_players).collect::<Vec<_>>();
+    let mut current_player = 0;
+    let mut marbles = vec![0];
+    let mut current = 0;
+    for m in 1..=last_marble {
+        if m % 23 == 0 {
+            current = (current + marbles.len() - 7) % marbles.len();
+            players[current_player] += m + marbles.remove(current);
+        } else {
+            current = (current + 2) % marbles.len();
+            marbles.insert(current, m);
+        }
+        current_player = (current_player + 1) % n_players;
+    }
+
+    let result = players.iter().max().unwrap();
+    println!("{}", result);
+}
---
+++ src/bin/a201809b.rs
@@ -0,0 +1,73 @@
+use std::collections::VecDeque;
+use std::io;
+use std::iter::repeat;
+use std::str::FromStr;
+
+use nom::call;
+use nom::digit;
+use nom::do_parse;
+use nom::error_position;
+use nom::map_res;
+use nom::named;
+use nom::tag;
+use nom::tag_s;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Config {
+    n_players: i64,
+    last_marble: i64,
+}
+
+named!(int64<&str, i64>,
+    map_res!(digit, FromStr::from_str)
+);
+
+named!(config<&str, Config>,
+       do_parse!(
+           n_players: int64
+               >> tag_s!(" players; last marble is worth ")
+               >> last_marble: int64
+               >> tag_s!(" points")
+               >> (Config{ n_players, last_marble })
+       )
+);
+
+fn main() {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).expect("I/O error");
+
+    // parse line
+    let result = config(&line.trim_end());
+    //println!("{:?}", result);
+
+    let config = result.unwrap().1;
+
+    let n_players = config.n_players as usize;
+    let last_marble = (config.last_marble as i64) * 100;
+
+    let mut players = repeat(0_i64).take(n_players).collect::<Vec<_>>();
+    let mut current_player = 0;
+    let mut marbles = VecDeque::new();
+    marbles.push_front(0);
+
+    // the current marble is the first in the linked list
+    for m in 1..=last_marble {
+        if m % 23 == 0 {
+            for _ in 0..7 {
+                let m1 = marbles.pop_back().unwrap();
+                marbles.push_front(m1);
+            }
+            players[current_player] += m + marbles.pop_front().unwrap();
+        } else {
+            for _ in 0..2 {
+                let m1 = marbles.pop_front().unwrap();
+                marbles.push_back(m1);
+            }
+            marbles.push_front(m);
+        }
+        current_player = (current_player + 1) % n_players;
+    }
+
+    let result = players.iter().max().unwrap();
+    println!("{}", result);
+}
