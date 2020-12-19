9952acc6deeb941e4941c27f3c15e15abc8dc97c
Juergen Stuber <juergen@jstuber.net>
2020-12-19 19:23:38 +0100

    Add 2020 days 18 and 19.

---
+++ a2020/src/bin/a202018.rs
@@ -0,0 +1,285 @@
+use core::str::FromStr;
+
+use std::fmt;
+use std::io;
+use std::io::Read;
+use std::rc::Rc;
+
+use nom::alt;
+use nom::char;
+use nom::digit;
+use nom::do_parse;
+use nom::line_ending;
+use nom::many0;
+use nom::many1;
+use nom::map_res;
+use nom::named;
+use nom::tag;
+use nom::value;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Op {
+    Add,
+    Mul,
+}
+impl Op {
+    fn apply(&self, a: i64, b: i64) -> i64 {
+        match self {
+            Op::Add => a + b,
+            Op::Mul => a * b,
+        }
+    }
+    fn to_char(&self) -> char {
+        match self {
+            Op::Add => '+',
+            Op::Mul => '*',
+        }
+    }
+}
+#[derive(Clone, Debug, PartialEq, Eq)]
+enum Expr {
+    Num(i64),
+    Paren(Rc<Expr>),
+    BinOps(Rc<Expr>, Vec<(Op, Rc<Expr>)>),
+}
+impl Expr {
+    fn from(e0: Expr, oes: Vec<(Op, Expr)>) -> Expr {
+        if oes.is_empty() {
+            e0
+        } else {
+            Expr::BinOps(
+                Rc::new(e0),
+                oes.into_iter()
+                    .map(|(op, e1)| (op, Rc::new(e1)))
+                    .collect::<Vec<_>>(),
+            )
+        }
+    }
+    fn eval1(&self) -> i64 {
+        match self {
+            Expr::Num(n) => *n,
+            Expr::Paren(e) => e.eval1(),
+            Expr::BinOps(e0, oes) => {
+                let mut v = e0.eval1();
+                for (op, e1) in oes {
+                    v = op.apply(v, e1.eval1());
+                }
+                v
+            }
+        }
+    }
+    fn eval2(&self) -> i64 {
+        match self {
+            Expr::Num(n) => *n,
+            Expr::Paren(e) => e.eval2(),
+            Expr::BinOps(e0, oes) => {
+                let mut sum = e0.eval2();
+                let mut prod = 1;
+                for (op, e1) in oes {
+                    let v1 = e1.eval2();
+                    match op {
+                        Op::Add => {
+                            sum += v1;
+                        }
+                        Op::Mul => {
+                            prod *= sum;
+                            sum = v1;
+                        }
+                    }
+                }
+                prod * sum
+            }
+        }
+    }
+}
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Num(n) => write!(f, "{}", n),
+            Expr::Paren(e) => write!(f, "({})", e),
+            Expr::BinOps(e0, oes) => {
+                write!(f, "{}", e0)?;
+                for (op, e1) in oes {
+                    write!(f, " {} {}", op.to_char(), e1)?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
+named!(int<&str, i64>,
+    map_res!(digit, FromStr::from_str)
+);
+named!(op<&str, Op>,
+    alt!(
+        value!(Op::Add, char!('+')) |
+        value!(Op::Mul, char!('*'))
+    )
+);
+named!(prim_expr<&str, Expr>,
+    alt!(
+        do_parse!(
+            char!('(') >>
+            e: expr >>
+            char!(')') >>
+                (Expr::Paren(Rc::new(e)))
+        ) |
+        do_parse!(
+            n: int >>
+                (Expr::Num(n))
+        )
+    )
+);
+named!(op_expr<&str, (Op, Expr)>,
+   do_parse!(
+       tag!(" ") >>
+       op: op >>
+       tag!(" ") >>
+       e: prim_expr >>
+           ((op, e))
+   )
+);
+named!(expr<&str, Expr>,
+    do_parse!(
+        e: prim_expr >>
+        oes: many0!(op_expr) >>
+            (Expr::from(e, oes))
+    )
+);
+named!(input<&str, Vec<Expr>>,
+    many1!(
+        do_parse!(
+            e: expr >>
+            line_ending >>
+                (e)
+        )
+    )
+);
+
+fn main() {
+    let mut input_data = String::new();
+    io::stdin()
+        .read_to_string(&mut input_data)
+        .expect("I/O error");
+
+    // make nom happy
+    input_data.push('\n');
+
+    // parse input
+    let result = input(&input_data);
+    //println!("{:?}", result);
+
+    let es = result.unwrap().1;
+    // for e in es {
+    //     println!("{}", e);
+    // }
+
+    let result_a = es.iter().map(|e| e.eval1()).sum::<i64>();
+    let result_b = es.iter().map(|e| e.eval2()).sum::<i64>();
+    println!("a: {}", result_a);
+    println!("b: {}", result_b);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use crate::expr;
+    use crate::Expr;
+
+    #[test]
+    fn test_expr_num() {
+        let e = Expr::Num(42);
+        assert_eq!(e, expr("42\n").unwrap().1);
+    }
+
+    #[test]
+    fn test_expr_paren() {
+        let e = Expr::Paren(Rc::new(Expr::Num(23)));
+        assert_eq!(e, expr("(23)\n").unwrap().1);
+    }
+
+    #[test]
+    fn test_eval1_sum2() {
+        let e = expr("2 + 3\n").unwrap().1;
+        assert_eq!(5, e.eval1());
+    }
+    #[test]
+    fn test_eval1_sum3() {
+        let e = expr("2 + 3 + 4\n").unwrap().1;
+        assert_eq!(9, e.eval1());
+    }
+
+    #[test]
+    fn test_eval1_mul2() {
+        let e = expr("2 * 3\n").unwrap().1;
+        assert_eq!(6, e.eval1());
+    }
+    #[test]
+    fn test_eval1_mul3() {
+        let e = expr("2 * 3 * 4\n").unwrap().1;
+        assert_eq!(24, e.eval1());
+    }
+
+    #[test]
+    fn test_eval1_mul_add() {
+        let e = expr("2 * 3 + 4\n").unwrap().1;
+        assert_eq!(10, e.eval1());
+    }
+    #[test]
+    fn test_eval1_add_mul() {
+        let e = expr("2 + 3 * 4\n").unwrap().1;
+        assert_eq!(20, e.eval1());
+    }
+
+    #[test]
+    fn test_eval1_26() {
+        let e = expr("2 * 3 + (4 * 5)\n").unwrap().1;
+        assert_eq!(26, e.eval1());
+    }
+    #[test]
+    fn test_eval1_437() {
+        let e = expr("5 + (8 * 3 + 9 + 3 * 4 * 3)\n").unwrap().1;
+        assert_eq!(437, e.eval1());
+    }
+    #[test]
+    fn test_eval1_12240() {
+        let e = expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))\n")
+            .unwrap()
+            .1;
+        assert_eq!(12240, e.eval1());
+    }
+    #[test]
+    fn test_eval1_13632() {
+        let e = expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2\n")
+            .unwrap()
+            .1;
+        assert_eq!(13632, e.eval1());
+    }
+
+    #[test]
+    fn test_eval2_46() {
+        let e = expr("2 * 3 + (4 * 5)\n").unwrap().1;
+        assert_eq!(46, e.eval2());
+    }
+    #[test]
+    fn test_eval2_1445() {
+        let e = expr("5 + (8 * 3 + 9 + 3 * 4 * 3)\n").unwrap().1;
+        assert_eq!(1445, e.eval2());
+    }
+    #[test]
+    fn test_eval2_669060() {
+        let e = expr("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))\n")
+            .unwrap()
+            .1;
+        assert_eq!(669060, e.eval2());
+    }
+    #[test]
+    fn test_eval2_23340() {
+        let e = expr("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2\n")
+            .unwrap()
+            .1;
+        assert_eq!(23340, e.eval2());
+    }
+}
---
+++ a2020/src/bin/a202019.rs
@@ -0,0 +1,254 @@
+use core::str::FromStr;
+
+use std::collections::HashMap;
+use std::fmt;
+use std::io;
+use std::io::Read;
+use std::rc::Rc;
+
+use nom::alpha;
+use nom::alt;
+use nom::char;
+use nom::digit;
+use nom::do_parse;
+use nom::line_ending;
+use nom::many1;
+use nom::map_res;
+use nom::named;
+use nom::none_of;
+use nom::separated_nonempty_list;
+use nom::tag;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+enum Expr {
+    Lit(char),
+    Rule(usize),
+    Seq(Rc<Expr>, Rc<Expr>),
+    Alt(Rc<Expr>, Rc<Expr>),
+}
+impl Expr {
+    fn from_sequence(seq: Vec<Expr>) -> Expr {
+        assert!(!seq.is_empty());
+        let mut iter = seq.into_iter();
+        let mut e0 = iter.next().unwrap();
+        for e1 in iter {
+            e0 = Expr::Seq(Rc::new(e0), Rc::new(e1));
+        }
+        e0
+    }
+    fn from_alternatives(alts: Vec<Expr>) -> Expr {
+        assert!(!alts.is_empty());
+        let mut iter = alts.into_iter();
+        let mut e0 = iter.next().unwrap();
+        for e1 in iter {
+            e0 = Expr::Alt(Rc::new(e0), Rc::new(e1));
+        }
+        e0
+    }
+}
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expr::Lit(c) => write!(f, "\"{}\"", c),
+            Expr::Rule(n) => write!(f, "{}", n),
+            Expr::Seq(e0, e1) => write!(f, "{} {}", e0, e1),
+            Expr::Alt(e0, e1) => write!(f, "{} | {}", e0, e1),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Rule {
+    number: usize,
+    body: Expr,
+}
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.number, self.body)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Input {
+    rules: Vec<Rule>,
+    messages: Vec<String>,
+}
+impl fmt::Display for Input {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for r in &self.rules {
+            writeln!(f, "{}", r)?;
+        }
+        writeln!(f)?;
+        for m in &self.messages {
+            writeln!(f, "{}", m)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Parser(HashMap<usize, Rc<Expr>>);
+impl Parser {
+    fn from(rules: Vec<Rule>) -> Parser {
+        Parser(
+            rules
+                .into_iter()
+                .map(|r| (r.number, Rc::new(r.body)))
+                .collect::<HashMap<_, _>>(),
+        )
+    }
+    fn matches(&self, rule_number: usize, msg: &str) -> bool {
+        let mut table = HashMap::new();
+        let msg = msg.chars().collect::<Vec<_>>();
+        let start = 0;
+        let end = msg.len();
+        self.parses_as_rule(&mut table, &msg, rule_number, start, end)
+    }
+    fn parses_as_rule(
+        &self,
+        table: &mut HashMap<(Rc<Expr>, usize, usize), bool>,
+        msg: &[char],
+        rule_number: usize,
+        start: usize,
+        end: usize,
+    ) -> bool {
+        let expr = self.get_rule(rule_number);
+        self.parses_as_expr(table, msg, expr, start, end)
+    }
+    fn parses_as_expr(
+        &self,
+        table: &mut HashMap<(Rc<Expr>, usize, usize), bool>,
+        msg: &[char],
+        expr: Rc<Expr>,
+        start: usize,
+        end: usize,
+    ) -> bool {
+        if let Some(&result) = table.get(&(expr.clone(), start, end)) {
+            result
+        } else {
+            let result = match &*expr {
+                Expr::Lit(c) => (end - start == 1 && msg[start] == *c),
+                Expr::Rule(n) => self.parses_as_rule(table, msg, *n, start, end),
+                Expr::Seq(e0, e1) => ((start + 1)..end).any(|mid| {
+                    self.parses_as_expr(table, msg, e0.clone(), start, mid)
+                        && self.parses_as_expr(table, msg, e1.clone(), mid, end)
+                }),
+                Expr::Alt(e0, e1) => {
+                    self.parses_as_expr(table, msg, e0.clone(), start, end)
+                        || self.parses_as_expr(table, msg, e1.clone(), start, end)
+                }
+            };
+            table.insert((expr, start, end), result);
+            result
+        }
+    }
+    fn get_rule(&self, rule_number: usize) -> Rc<Expr> {
+        self.0[&rule_number].clone()
+    }
+}
+
+named!(int<&str, usize>,
+    map_res!(digit, FromStr::from_str)
+);
+named!(literal<&str, Expr>,
+    do_parse!(
+        char!('\"') >>
+        c: none_of!("\"") >>
+        char!('\"') >>
+            (Expr::Lit(c))
+    )
+);
+named!(sequence<&str, Expr>,
+    do_parse!(
+        seq: separated_nonempty_list!(tag!(" "), int) >>
+            (Expr::from_sequence(
+                seq.into_iter()
+                    .map(|n| Expr::Rule(n))
+                    .collect::<Vec<_>>()
+            ))
+    )
+);
+named!(alternatives<&str, Expr>,
+    do_parse!(
+        alts: separated_nonempty_list!(tag!(" | "), sequence) >>
+            (Expr::from_alternatives(alts))
+    )
+);
+named!(body<&str, Expr>,
+    alt!(
+        literal |
+        alternatives
+    )
+);
+named!(rule<&str, Rule>,
+    do_parse!(
+        number: int >>
+        tag!(": ") >>
+        body: body >>
+        line_ending >>
+            (Rule { number, body })
+    )
+);
+named!(message<&str, String>,
+    do_parse!(
+        msg: alpha >>
+        line_ending >>
+            (String::from(msg))
+    )
+);
+named!(input<&str, Input>,
+    do_parse!(
+        rules: many1!(rule) >>
+        line_ending >>
+        messages: many1!(message) >>
+            (Input { rules, messages })
+    )
+);
+
+fn main() {
+    let mut input_data = String::new();
+    io::stdin()
+        .read_to_string(&mut input_data)
+        .expect("I/O error");
+
+    // make nom happy
+    input_data.push('\n');
+
+    // parse input
+    let result = input(&input_data);
+    //println!("{:?}", result);
+
+    let input = result.unwrap().1;
+    //println!("{}", input);
+
+    let parser_a = Parser::from(input.rules.clone());
+    let result_a = input
+        .messages
+        .iter()
+        .filter(|msg| parser_a.matches(0, msg))
+        .count();
+
+    let parser_b = Parser::from(
+        input
+            .rules
+            .into_iter()
+            .map(|r| {
+                if r.number == 8 {
+                    rule("8: 42 | 42 8\n").unwrap().1
+                } else if r.number == 11 {
+                    rule("11: 42 31 | 42 11 31\n").unwrap().1
+                } else {
+                    r
+                }
+            })
+            .collect::<Vec<_>>(),
+    );
+    let result_b = input
+        .messages
+        .iter()
+        .filter(|msg| parser_b.matches(0, msg))
+        .count();
+
+    println!("a: {}", result_a);
+    println!("b: {}", result_b);
+}
