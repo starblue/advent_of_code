ecc5b93501046c0aa4b7a0afb747e5b31ff13924
Juergen Stuber <juergen@jstuber.net>
2021-01-07 19:28:32 +0100

    Use gamedim for 2019 day 12.

--- a2019/src/bin/a201912.rs
+++ a2019/src/bin/a201912.rs
@@ -9,69 +9,61 @@ use nom::*;
 
 use num::integer::lcm;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-struct Vec3d {
-    pub x: i32,
-    pub y: i32,
-    pub z: i32,
-}
+use gamedim::p3d;
+use gamedim::v3d;
+use gamedim::Point3d;
+use gamedim::Vec3d;
+use gamedim::Vector;
 
 named!(
-    int32<&str, i32>,
+    int<&str, i64>,
     map_res!(recognize!(tuple!(opt!(char!('-')), digit)), FromStr::from_str)
 );
 
 named!(
-    line<&str, Vec3d>,
+    line<&str, Point3d>,
     do_parse!(
         tag!("<x=") >>
-        x: int32 >>
+        x: int >>
         tag!(", y=") >>
-        y: int32 >>
+        y: int >>
         tag!(", z=") >>
-        z: int32 >>
+        z: int >>
         tag!(">") >>
-        line_ending >> (Vec3d{ x, y, z })
+        line_ending >> (p3d(x, y, z))
     )
 );
 
 named!(
-    input<&str, Vec<Vec3d>>,
+    input<&str, Vec<Point3d>>,
     many1!(line)
 );
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct State {
-    ps: Vec<Vec3d>,
+    ps: Vec<Point3d>,
     vs: Vec<Vec3d>,
 }
 impl State {
-    fn new(ps: &Vec<Vec3d>) -> State {
+    fn new(ps: &Vec<Point3d>) -> State {
         State {
             ps: ps.to_owned(),
-            vs: repeat(Vec3d { x: 0, y: 0, z: 0 })
-                .take(ps.len())
-                .collect::<Vec<_>>(),
+            vs: repeat(v3d(0, 0, 0)).take(ps.len()).collect::<Vec<_>>(),
         }
     }
     fn step(&mut self) {
         // apply gravity
         for i in 0..self.ps.len() {
-            let mut a = Vec3d { x: 0, y: 0, z: 0 };
-            for j in 0..self.ps.len() {
-                a.x += (self.ps[j].x - self.ps[i].x).signum();
-                a.y += (self.ps[j].y - self.ps[i].y).signum();
-                a.z += (self.ps[j].z - self.ps[i].z).signum();
+            let p0 = self.ps[i];
+            let mut a = v3d(0, 0, 0);
+            for p1 in self.ps.iter() {
+                a += (p1 - p0).signum();
             }
-            self.vs[i].x += a.x;
-            self.vs[i].y += a.y;
-            self.vs[i].z += a.z;
+            self.vs[i] += a;
         }
         // apply velocity
         for i in 0..self.vs.len() {
-            self.ps[i].x += self.vs[i].x;
-            self.ps[i].y += self.vs[i].y;
-            self.ps[i].z += self.vs[i].z;
+            self.ps[i] += self.vs[i];
         }
     }
     #[allow(unused)]
@@ -81,16 +73,23 @@ impl State {
             let v = self.vs[i];
             print!(
                 "{:1}({:4},{:4},{:4}) ({:4},{:4},{:4}) / ",
-                i, p.x, p.y, p.z, v.x, v.y, v.z
+                i,
+                p.x(),
+                p.y(),
+                p.z(),
+                v.x(),
+                v.y(),
+                v.z()
             );
         }
         println!();
     }
-    fn energy(&self) -> i32 {
+    fn energy(&self) -> i64 {
+        let p0 = p3d(0, 0, 0);
         let mut energy = 0;
         for i in 0..self.ps.len() {
-            let potential_energy = self.ps[i].x.abs() + self.ps[i].y.abs() + self.ps[i].z.abs();
-            let kinetic_energy = self.vs[i].x.abs() + self.vs[i].y.abs() + self.vs[i].z.abs();
+            let potential_energy = (self.ps[i] - p0).norm_l1();
+            let kinetic_energy = self.vs[i].norm_l1();
             energy += potential_energy * kinetic_energy;
         }
         energy
@@ -99,11 +98,11 @@ impl State {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct State1 {
-    ps: Vec<i32>,
-    vs: Vec<i32>,
+    ps: Vec<i64>,
+    vs: Vec<i64>,
 }
 impl State1 {
-    fn new(ps: &Vec<i32>) -> State1 {
+    fn new(ps: &Vec<i64>) -> State1 {
         State1 {
             ps: ps.to_owned(),
             vs: repeat(0).take(ps.len()).collect::<Vec<_>>(),
@@ -125,7 +124,7 @@ impl State1 {
     }
 }
 
-fn detect_loop(ps: Vec<i32>) -> (i64, i64) {
+fn detect_loop(ps: Vec<i64>) -> (i64, i64) {
     let mut state = State1::new(&ps);
     let mut first_seen = HashMap::new();
     let mut count = 0;
@@ -161,11 +160,11 @@ fn main() {
     }
 
     // detect loop separately per coordinate
-    let xs = ps.iter().map(|p| p.x).collect::<Vec<_>>();
+    let xs = ps.iter().map(|p| p.x()).collect::<Vec<_>>();
     let (c0x, c1x) = detect_loop(xs);
-    let ys = ps.iter().map(|p| p.y).collect::<Vec<_>>();
+    let ys = ps.iter().map(|p| p.y()).collect::<Vec<_>>();
     let (c0y, c1y) = detect_loop(ys);
-    let zs = ps.iter().map(|p| p.z).collect::<Vec<_>>();
+    let zs = ps.iter().map(|p| p.z()).collect::<Vec<_>>();
     let (c0z, c1z) = detect_loop(zs);
 
     assert_eq!(0, c0x);
