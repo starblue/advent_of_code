cd134e86c26a3333b9969ba94d70a1d061bc996e
Juergen Stuber <juergen@jstuber.net>
2021-01-09 13:52:09 +0100

    Use gamedim for 2018 day 6.

--- a2018/src/bin/a201806.rs
+++ a2018/src/bin/a201806.rs
@@ -9,38 +9,16 @@ use nom::map_res;
 use nom::named;
 use nom::ws;
 
-#[derive(Clone, Copy, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
-}
+use gamedim::p2d;
+use gamedim::Point2d;
 
-impl Point {
-    fn distance(&self, p1: &Point) -> i32 {
-        (p1.x - self.x).abs() + (p1.y - self.y).abs()
-    }
-
-    fn is_east_of(&self, p1: &Point) -> bool {
-        p1.x - self.x > (p1.y - self.y).abs()
-    }
-    fn is_north_of(&self, p1: &Point) -> bool {
-        p1.y - self.y > (p1.x - self.x).abs()
-    }
-    fn is_west_of(&self, p1: &Point) -> bool {
-        self.x - p1.x > (p1.y - self.y).abs()
-    }
-    fn is_south_of(&self, p1: &Point) -> bool {
-        self.y - p1.y > (p1.x - self.x).abs()
-    }
-}
-
-named!(int32<&str, i32>,
+named!(int<&str, i64>,
     map_res!(digit, FromStr::from_str)
 );
 
 named!(
-    position<&str, Point>,
-    do_parse!(x: int32 >> ws!(char!(',')) >> y: int32 >> (Point { x, y }))
+    position<&str, Point2d>,
+    do_parse!(x: int >> ws!(char!(',')) >> y: int >> (p2d(x, y)))
 );
 
 fn main() {
@@ -65,10 +43,10 @@ fn main() {
     let mut counts = repeat(0).take(positions.len()).collect::<Vec<_>>();
     for x in 0..1000 {
         for y in 0..1000 {
-            let mut min_d = std::i32::MAX;
+            let mut min_d = std::i64::MAX;
             let mut min_i = None;
             for (i, p) in positions.iter().enumerate() {
-                let d = p.distance(&Point { x, y });
+                let d = p.distance_l1(p2d(x, y));
                 if d < min_d {
                     min_d = d;
                     min_i = Some(i);
@@ -87,17 +65,31 @@ fn main() {
 
     let mut count_max = 0;
     for (p, &c) in positions.iter().zip(counts.iter()) {
-        let e_finite = positions.iter().any(|p1| p1.is_east_of(p));
-        let n_finite = positions.iter().any(|p1| p1.is_north_of(p));
-        let w_finite = positions.iter().any(|p1| p1.is_west_of(p));
-        let s_finite = positions.iter().any(|p1| p1.is_south_of(p));
-        if e_finite && n_finite && w_finite && s_finite {
+        let pos_x_finite = positions.iter().any(|p1| (p1 - p).is_towards_pos_x());
+        let neg_x_finite = positions.iter().any(|p1| (p1 - p).is_towards_neg_x());
+        let pos_y_finite = positions.iter().any(|p1| (p1 - p).is_towards_pos_y());
+        let neg_y_finite = positions.iter().any(|p1| (p1 - p).is_towards_neg_y());
+        if pos_x_finite && neg_x_finite && pos_y_finite && neg_y_finite {
             // area is finite
             if c > count_max {
                 count_max = c;
             }
         }
     }
+    println!("a: {}", count_max);
 
-    println!("{}", count_max);
+    let mut count = 0;
+    for x in 0..1000 {
+        for y in 0..1000 {
+            let total_distance = positions
+                .iter()
+                .map(|p| p.distance_l1(p2d(x, y)))
+                .sum::<i64>();
+            if total_distance < 10000 {
+                count += 1;
+            }
+        }
+    }
+
+    println!("b: {}", count);
 }
