f28a389e594dab7e6b0be6ce87b7488c4a472c0a
Juergen Stuber <juergen@jstuber.net>
2021-07-30 13:59:36 +0200

    Optimize 2017 day 17.
    Replace the linked list by a VecDeque and use rotate_left().

--- a2017/src/bin/a201717b.rs
+++ a2017/src/bin/a201717b.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use std::collections::LinkedList;
+use std::collections::VecDeque;
 
 const N: usize = 50_000_000;
 
@@ -9,17 +9,15 @@ fn main() {
     io::stdin().read_line(&mut line).expect("I/O error");
 
     let step: usize = line.trim().parse().unwrap();
+    //println!("step = {}", step);
 
-    let mut vs = LinkedList::new();
+    let mut vs = VecDeque::new();
     vs.push_back(0);
     for i in 1..(N + 1) {
-        let s = step % vs.len();
-        let mut vs_end = vs.split_off(s);
-        vs_end.append(&mut vs);
-        vs = vs_end;
+        vs.rotate_left(step % vs.len());
         vs.push_back(i);
 
-        if i % 1_000_000 == 0 {
+        if i % 10_000_000 == 0 {
             println!("{}", i);
         }
     }
