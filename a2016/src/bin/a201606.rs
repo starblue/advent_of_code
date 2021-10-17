76874cb55669a2f4b1f535b2f7c2218d5b443df8
Juergen Stuber <juergen@jstuber.net>
2021-10-17 14:14:35 +0200

    Add 2016 day 6

---
+++ a2016/src/bin/a201606.rs
@@ -0,0 +1,75 @@
+use std::collections::HashMap;
+use std::io;
+use std::io::Read;
+
+use nom::character::complete::alpha1;
+use nom::character::complete::line_ending;
+use nom::combinator::map;
+use nom::combinator::recognize;
+use nom::multi::many1;
+use nom::IResult;
+
+fn message(i: &str) -> IResult<&str, String> {
+    map(recognize(alpha1), String::from)(i)
+}
+
+fn line(i: &str) -> IResult<&str, String> {
+    let (i, message) = message(i)?;
+    let (i, _) = line_ending(i)?;
+    Ok((i, message))
+}
+
+fn input(i: &str) -> IResult<&str, Vec<String>> {
+    many1(line)(i)
+}
+
+fn main() {
+    let mut input_data = String::new();
+    io::stdin()
+        .read_to_string(&mut input_data)
+        .expect("I/O error");
+
+    // make nom happy
+    input_data.push('\n');
+
+    // parse input
+    let result = input(&input_data);
+    //println!("{:?}", result);
+
+    let input = result.unwrap().1;
+    // for msg in &input {
+    //     println!("{}", msg);
+    // }
+
+    let len = input.iter().map(|s| s.len()).max().unwrap();
+    let mut counts = (0..len).map(|_| HashMap::new()).collect::<Vec<_>>();
+    for msg in &input {
+        for (i, c) in msg.chars().enumerate() {
+            let map = &mut counts[i];
+            let entry = map.entry(c).or_insert(0);
+            *entry += 1;
+        }
+    }
+    let result_a = counts
+        .iter()
+        .map(|m| {
+            m.iter()
+                .max_by_key(|(_, count)| *count)
+                .map(|(ch, _)| ch)
+                .unwrap()
+        })
+        .collect::<String>();
+
+    let result_b = counts
+        .iter()
+        .map(|m| {
+            m.iter()
+                .min_by_key(|(_, count)| *count)
+                .map(|(ch, _)| ch)
+                .unwrap()
+        })
+        .collect::<String>();
+
+    println!("a: {}", result_a);
+    println!("b: {}", result_b);
+}
