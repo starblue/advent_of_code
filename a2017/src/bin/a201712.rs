0f6752451a98f036f2051ddcb2e0ec7364f21d12
Juergen Stuber <juergen@jstuber.net>
2021-11-13 13:38:32 +0100

    Add solution for 2017 day 12

---
+++ a2017/src/bin/a201712.rs
@@ -0,0 +1,124 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fmt;
+use std::io;
+use std::io::Read;
+use std::str::FromStr;
+
+use nom::bytes::complete::tag;
+use nom::character::complete::digit1;
+use nom::character::complete::line_ending;
+use nom::combinator::map_res;
+use nom::combinator::recognize;
+use nom::multi::many1;
+use nom::multi::separated_list1;
+use nom::IResult;
+
+#[derive(Clone, Debug)]
+struct Program {
+    id: usize,
+    neighbors: Vec<usize>,
+}
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.id)?;
+        let mut sep = " <-> ";
+        for n in &self.neighbors {
+            write!(f, "{}{}", sep, n)?;
+            sep = ", ";
+        }
+        Ok(())
+    }
+}
+
+fn uint(i: &str) -> IResult<&str, usize> {
+    map_res(recognize(digit1), FromStr::from_str)(i)
+}
+
+fn program(i: &str) -> IResult<&str, Program> {
+    let (i, id) = uint(i)?;
+    let (i, _) = tag(" <-> ")(i)?;
+    let (i, neighbors) = separated_list1(tag(", "), uint)(i)?;
+    let (i, _) = line_ending(i)?;
+    Ok((i, Program { id, neighbors }))
+}
+
+fn input(i: &str) -> IResult<&str, Vec<Program>> {
+    many1(program)(i)
+}
+
+fn find(reprs: &mut HashMap<usize, usize>, i: usize) -> usize {
+    let mut i = i;
+    while reprs[&i] != i {
+        let prev = i;
+        i = reprs[&i];
+        let j = reprs[&i];
+        let entry = reprs.entry(prev).or_default();
+        *entry = j;
+    }
+    i
+}
+
+fn union(reprs: &mut HashMap<usize, usize>, i: usize, j: usize) {
+    let i_repr = find(reprs, i);
+    let j_repr = find(reprs, j);
+    // Use the smaller representative for both.
+    match i_repr.cmp(&j_repr) {
+        Ordering::Less => {
+            let entry = reprs.entry(j_repr).or_default();
+            *entry = i_repr;
+        }
+        Ordering::Greater => {
+            let entry = reprs.entry(i_repr).or_default();
+            *entry = j_repr;
+        }
+        Ordering::Equal => {
+            // Both representatives are already equal, nothing needs to be done.
+        }
+    }
+}
+
+fn main() {
+    let mut input_data = String::new();
+    io::stdin()
+        .read_to_string(&mut input_data)
+        .expect("I/O error");
+
+    // make nom happy
+    input_data.push('\n');
+
+    // parse input
+    let result = input(&input_data);
+    //println!("{:?}", result);
+
+    let input = result.unwrap().1;
+    // for program in &input {
+    //     println!("{}", program);
+    // }
+
+    // Use union-find to represent each groups by the smallest id of
+    // a program in the group.
+    // This guarantees that the group containing program 0 is represented by 0.
+    // We use a hash map, since we do not want to depend on the ids
+    // being consecutive.
+    let mut reprs = input
+        .iter()
+        .map(|p| (p.id, p.id))
+        .collect::<HashMap<_, _>>();
+    for p in &input {
+        for &n_id in &p.neighbors {
+            union(&mut reprs, p.id, n_id);
+        }
+    }
+    let result_a = input.iter().filter(|p| find(&mut reprs, p.id) == 0).count();
+
+    let group_ids = input
+        .iter()
+        .map(|p| find(&mut reprs, p.id))
+        .collect::<HashSet<_>>();
+    let result_b = group_ids.len();
+
+    println!("a: {}", result_a);
+    println!("b: {}", result_b);
+}
