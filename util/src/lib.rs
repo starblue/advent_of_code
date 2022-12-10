dfb6415a0c4bc5a1bbb7d7b4b69a52154318cc3d
Juergen Stuber <juergen@jstuber.net>
2022-12-10 13:34:21 +0100

    Use function to create runtime errors with type Box<dyn Error>

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,8 +1,16 @@
+use std::error;
+
 mod runtime_error;
+pub fn make_runtime_error(message: String) -> Box<dyn error::Error> {
+    Box::new(RuntimeError::new(message))
+}
 #[macro_export]
 macro_rules! runtime_error {
     ($($t:tt)*) => {
-        RuntimeError::new(format!($($t)*))
+        {
+            use util::make_runtime_error;
+            make_runtime_error(format!($($t)*))
+        }
     };
 }
 pub use runtime_error::RuntimeError;
